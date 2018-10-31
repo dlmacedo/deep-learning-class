f48cc6cafc5fabe3c13a2fc9b5a1451cb2a934a1
dlmacedo <dlm@cin.ufpe.br>
2018-10-31 02:53:47 +0000

    Better run.sh for GANs...

--- generative-adversarial-networks/run.sh
+++ generative-adversarial-networks/run.sh
@@ -1,1 +1,7 @@
-python main.py --dataset fashion-mnist --gan_type infoGAN --epoch 50 --batch_size 64
+python main.py --dataset mnist --gan_type GAN --epoch 25 --batch_size 64
+#python main.py --dataset mnist --gan_type CGAN --epoch 25 --batch_size 64
+#python main.py --dataset mnist --gan_type infoGAN --epoch 25 --batch_size 64
+
+#python main.py --dataset fashion-mnist --gan_type GAN --epoch 25 --batch_size 64
+#python main.py --dataset fashion-mnist --gan_type CGAN --epoch 25 --batch_size 64
+#python main.py --dataset fashion-mnist --gan_type infoGAN --epoch 25 --batch_size 64
