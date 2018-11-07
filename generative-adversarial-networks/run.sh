c6e21c4cf7e184669171a1bdc93ace20b00590ec
dlmacedo <dlm@cin.ufpe.br>
2018-11-07 13:56:49 +0000

    Running many GANs...

--- generative-adversarial-networks/run.sh
+++ generative-adversarial-networks/run.sh
@@ -1,7 +1,7 @@
-python main.py --dataset mnist --gan_type GAN --epoch 25 --batch_size 64
-#python main.py --dataset mnist --gan_type CGAN --epoch 25 --batch_size 64
-#python main.py --dataset mnist --gan_type infoGAN --epoch 25 --batch_size 64
+python main.py --dataset mnist --gan_type GAN --epoch 5 --batch_size 64
+python main.py --dataset mnist --gan_type CGAN --epoch 5 --batch_size 64
+python main.py --dataset mnist --gan_type infoGAN --epoch 5 --batch_size 64
 
-#python main.py --dataset fashion-mnist --gan_type GAN --epoch 25 --batch_size 64
-#python main.py --dataset fashion-mnist --gan_type CGAN --epoch 25 --batch_size 64
-#python main.py --dataset fashion-mnist --gan_type infoGAN --epoch 25 --batch_size 64
+python main.py --dataset fashion-mnist --gan_type GAN --epoch 5 --batch_size 64
+python main.py --dataset fashion-mnist --gan_type CGAN --epoch 5 --batch_size 64
+python main.py --dataset fashion-mnist --gan_type infoGAN --epoch 5 --batch_size 64
